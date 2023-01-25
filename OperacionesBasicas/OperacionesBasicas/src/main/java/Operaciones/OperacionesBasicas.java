26a0e39dcccecc964c03f34ddcc96d01e8dcb6e8
ronahect29 <ronahect29@gmail.com>
2023-01-24 22:27:56 -0600

    cambios del programa operaciones basicas

--- OperacionesBasicas/OperacionesBasicas/src/main/java/Operaciones/OperacionesBasicas.java
+++ OperacionesBasicas/OperacionesBasicas/src/main/java/Operaciones/OperacionesBasicas.java
@@ -19,14 +19,15 @@ public class OperacionesBasicas {
     public static void main(String[] args) {
         // TODO code application logic here
         float opc=0,dato1=0,dato2=0,result=0;
-        System.out.println("seleccione una opcion: ");
-        System.out.println("1.-sumar    2.-restar   3.-multiplicar  4.-dividir  otro numero.-salir");
+        JOptionPane.showMessageDialog(null,"Operaciones Basicas ");
+       // JOptionPane.showInputDialog("seleccione una opcion:  1. -sumar  2. -restar  3-multiplicar  4-dividir  otro numeero.-salir");
+        
        
-        opc=Integer.parseInt(JOptionPane.showInputDialog("ingrese opcion: "));
+        opc=Integer.parseInt(JOptionPane.showInputDialog("seleccione una opcion: 1. -sumar  2. -restar  3-multiplicar  4-dividir  otro numeero.-salir\""));
 
         if(opc>0&&opc<5){
             dato1=Integer.parseInt(JOptionPane.showInputDialog("ingrese primer numero: "));
-            dato2=Integer.parseInt(JOptionPane.showInputDialog("ingrese segundo numero numero: "));
+            dato2=Integer.parseInt(JOptionPane.showInputDialog("ingrese segundo numero"));
         if(opc!=0){
             if(opc==1)
                 result=dato1+dato2;
@@ -42,7 +43,7 @@ public class OperacionesBasicas {
         JOptionPane.showMessageDialog(null, "el resultado es: "+result);
         }
             System.out.println("saliendo...");
-            JOptionPane.showMessageDialog(null,"adios");        
+            JOptionPane.showMessageDialog(null,"adios");      
     }
     
 }
